887e57945db9f8beda5d951623120fed27893934
ITtechnical <68113347+ITtechnical@users.noreply.github.com>
2021-02-26 14:57:16 +0000

    Update settings.py

--- amis/settings.py
+++ amis/settings.py
@@ -135,14 +135,7 @@ LOGIN_URL = 'helpdesk:login'
 
 
 
-#SMTP Configuration
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST ='Smtp.office365.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER ='#########'
-# EMAIL_HOST_PASSWORD = '############'
 
 
 EMAIL_HOST ='smtp.gmail.com'
